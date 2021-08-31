99af1cdc67de1649ec6c66a6ecc3b66c8db9f70d
Shakirat komolafe <damolakomolafe.dk@gmail.com>
2021-08-31 16:22:04 -0400

    add functionality to scheduler app

---
+++ src/components/DayList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+export default function DayList() {
+  return (
+  <ul></ul>
+   
+  );
+}
---
+++ src/components/DayListItem.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import classNames from 'classnames';
+
+import "components/DayListItem.scss";
+
+function formatSpots(props){
+  if (props.spots === 0){
+   return "no spots remaining";
+  }
+  if (props.spots === 1){
+   return "1 spot remaining";
+ }
+ return `${props.spots} spots remaining`;
+  
+}
+
+export default function DayListItem(props) {
+
+   let dayClass = classNames("day-list__item",{
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+
+  })
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}> 
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3  className="text--light">{formatSpots(props)}</h3>
+    </li>
+  );
+}
