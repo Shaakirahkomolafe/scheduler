addc09c08bc3753fa97212f9e043fa5d1c19b685
Shakirat komolafe <damolakomolafe.dk@gmail.com>
2021-09-14 20:08:23 -0400

    fix failing test

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -8,12 +8,24 @@ export default function useVisualMode(initial) {
 //function to transition through different states
   function transition(mode, replace = false) {
     setMode(mode)
-    if (replace === true) {
-      setHistory((prev) => [...prev.slice(0, prev.length-1), mode])
+      if (!replace) {
+         setHistory(prev => {
+            return [...prev, mode]
+          })
+       } else {
+         setHistory(prev => {
+            return [...prev.slice(0, prev.length - 1), mode]
+         })
+        }
    }
-    setHistory((prev) => [...prev, mode])
-  }
-  //function to go back to a previous state
+   
+ //   setMode(mode)s
+  //   if (replace === true) {
+  //     setHistory((prev) => [...prev.slice(0, prev.length-1), mode])
+  //  }
+  //   setHistory((prev) => [...prev, mode])
+  // }
+  // //function to go back to a previous state
 function back(){
 if(history.length > 1){
   history.pop()
